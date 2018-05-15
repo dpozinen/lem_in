46d794223e29af52867401259ff9a9b81ade169c
Dariy POZINENKO <dpozinen@e1r2p16.unit.ua>
2018-05-15 15:37:22 +0300

    header

--- lem_in.h
+++ lem_in.h
@@ -1,19 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   read.c		                                        :+:      :+:    :+:   */
+/*   lem_in.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpozinen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/15 14:53:56 by dpozinen          #+#    #+#             */
-/*   Updated: 2018/05/15 14:53:57 by dpozinen         ###   ########.fr       */
+/*   Created: 2018/05/15 14:44:53 by dpozinen          #+#    #+#             */
+/*   Updated: 2018/05/15 14:44:54 by dpozinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lem_in.h"
+#ifndef LEM_IN_H
+# define LEM_IN_H
 
-void	read_input(t_main *lem)
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+
+typedef struct		s_room
 {
-	char *s;
+	char			*name;
+	int				index;
+}					t_room;
 
-}
+typedef struct		s_main
+{
+	unsigned int	ants;
+	char			*start;
+	char			*end;
+	int				best_result;
+	int				**rooms_matrix;
+	t_room			**start;
+}					t_main;
+
+#endif
